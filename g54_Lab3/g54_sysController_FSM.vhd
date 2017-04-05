d7c9745eaf41990e14c554c4347f261048b68717
Eva Suska <eva.suska@mail.mcgill.ca>
2017-04-05 01:10:44 -0400

    full testbed added

--- g54_Lab3/g54_sysController_FSM.vhd
+++ g54_Lab3/g54_sysController_FSM.vhd
@@ -13,8 +13,8 @@ port (Clk : in std_logic;
 		RST: in std_logic;
 		CNT: in std_logic_vector (3 downto 0);
 		stack_en: in std_logic;
-		player_num: in std_logic;
-		comp_num: in std_logic;
+		player_empty: in std_logic;
+		comp_empty: in std_logic;
 		in_turn: in std_logic;
 		request_deal: in std_logic;
 		done_turn: in std_logic;
@@ -23,12 +23,13 @@ port (Clk : in std_logic;
 		TURN : out std_logic;
 		start : out std_logic;
 		deal : out std_logic;
-		d_turn : out std_logic);
+		d_turn : out std_logic;
+		init_dealer : out std_logic);
 end g54_sysController_FSM;
 
 architecture controller of g54_sysController_FSM is
 
-type state_signal is (START_GAME, DEAL_COMP, DEAL_PLAYER, DEAL_1ST_CARD, PICK_START_PLAYER, COMP_TURN, PLAYER_TURN, CARD_COMP, CARD_PLAYER, GAME_OVER);
+type state_signal is (START_GAME, INIT, DEAL_COMP, DEAL_PLAYER, DEAL_1ST_CARD, PICK_START_PLAYER, COMP_TURN, PLAYER_TURN, CARD_COMP, CARD_PLAYER, GAME_OVER);
 
 signal state: state_signal;
 
@@ -39,7 +40,7 @@ state_update: process(CLK)
 begin
 
 	-- waiting to start game
-	if RST = '0' then
+	if RST = '1' then
 		state <= START_GAME;
 		
 	elsif CLK'event and CLK = '1' then
@@ -48,7 +49,11 @@ begin
 
 		when START_GAME =>
 
-			if RST = '1' then state <= DEAL_COMP; end if;
+			if RST = '0' then state <= INIT; end if;
+			
+		when INIT =>
+			
+			state <= DEAL_COMP;
 
 		when DEAL_COMP =>
 
@@ -70,13 +75,13 @@ begin
 		when COMP_TURN =>
 
 			if request_deal = '1' then state <= CARD_COMP; end if;
-			if player_num = '0' then state <= GAME_OVER; end if;
+			if player_empty = '1' then state <= GAME_OVER; end if;
 			if done_turn = '1' then state <= PLAYER_TURN; end if;
 
 		when PLAYER_TURN =>
 
 			if request_deal = '1' then state <= CARD_PLAYER; end if;
-			if comp_num = '0' then state <= GAME_OVER; end if;
+			if comp_empty = '1' then state <= GAME_OVER; end if;
 			if player_popped = '1' then state <= COMP_TURN; end if;
 			
 		when CARD_COMP =>
@@ -106,60 +111,77 @@ begin
 				start <= '0';
 				deal <= '0';
 				d_turn <= '0';
-				
+				init_dealer <= '0';
+
+			when INIT => 
+				TURN <= '0';
+				start <= '0';
+				deal <= '0';
+				d_turn <= '0';
+				init_dealer <= '1';
+
 			when DEAL_COMP => 
 				TURN <= '0';
 				start <= '0';
 				deal <= '1';
 				d_turn <= '1';
+				init_dealer <= '0';				
 
 			when DEAL_PLAYER => 
 				TURN <= '0';
 				start <= '0';
 				deal <= '1';
 				d_turn <= '0';
+				init_dealer <= '0';
 
 			when DEAL_1ST_CARD => 
 				TURN <= '0';
 				start <= '1';
 				deal <= '0';
 				d_turn <= '0';
+				init_dealer <= '0';
 
 			when PICK_START_PLAYER => 
 				TURN <= '0';
 				start <= '0';
 				deal <= '0';
 				d_turn <= '0';
+				init_dealer <= '0';
 
 			when COMP_TURN => 
 				TURN <= '1';
 				start <= '0';
 				deal <= '0';
 				d_turn <= '1';
+				init_dealer <= '0';
 
 			when PLAYER_TURN => 
 				TURN <= '0';
 				start <= '0';
 				deal <= '0';
 				d_turn <= '0';
+				init_dealer <= '0';
 
 			when CARD_COMP => 
 				TURN <= '1';
 				start <= '0';
 				deal <= '1';
 				d_turn <= '1';
+				init_dealer <= '0';
 
 			when CARD_PLAYER => 
 				TURN <= '0';
 				start <= '0';
 				deal <= '1';
 				d_turn <= '0';
+				init_dealer <= '0';
 
 			when GAME_OVER => 
 				TURN <= '0';
 				start <= '0';
 				deal <= '0';
 				d_turn <= '0';			
+				init_dealer <= '0';
 
 		end case;
 
